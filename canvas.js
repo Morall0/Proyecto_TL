61843ca404f8e1d908bc8c49d5774ae372e8148d
Mauricio ML <mau_ml@comunidad.unam.mx>
2023-06-09 23:26:45 -0600

    Rotaciones corregidas con composicion y angulos

--- canvas.js
+++ canvas.js
@@ -20,8 +20,12 @@
 }*/
 
 // Funcion que aplica la T de rotacion a un arreglo de coordenadas.
-function transformacionRot3D(coords, ang, A) {
+function transformacionRot3D(coords, a, b, g) {
     let nuevas_coords= JSON.parse(JSON.stringify(coords)); // Se saca la copia de las coordenadas originales.
+
+    const A = [[Math.cos(b)*Math.cos(g), -Math.cos(b)*Math.sin(g), Math.sin(b)],
+             [Math.sin(a)*Math.sin(g)-Math.cos(a)*Math.sin(b)*Math.cos(g), Math.cos(a)*Math.sin(b)*Math.sin(g)+Math.sin(a)*Math.cos(g), Math.cos(a)*Math.cos(b)],
+             [Math.sin(a)*Math.sin(b)*Math.cos(g)+Math.cos(a)*Math.sin(g), Math.cos(a)*Math.cos(g)-Math.sin(a)*Math.sin(b)*Math.sin(g), -Math.sin(a)*Math.cos(b)]];
 
     coords.forEach((B, index) => { // Por cada par de coordenadas...
         for(let n=0; n<A.length; n++) { // Itera filas de matriz A
@@ -36,6 +40,7 @@ function transformacionRot3D(coords, ang, A) {
     return nuevas_coords;
 }
 
+// T:R3->R2 donde T(x, y, z) = (x, y)
 function transformacionR3aR2(coords) {
     coords_2d=[];
     coords.forEach((coord)=>{
@@ -63,24 +68,22 @@ function dibuja2d (ctx, coords) {
 const canvas = document.getElementById("canvas"); // Se enlaza el canvas del html.
 const ctx2d = canvas.getContext("2d"); // Obteniendo el contexto (se trabaja en un plano 2d)
 
-const coords=[[-150, 150, 150],[150, 150, 150],[150, -150, 150],[-150, -150, 150], [-150, 150, -150],[150, 150, -150],[150, -150, -150],[-150, -150, -150]];
-
 let cubo = {
-    cara_i: [[-150, 150, 150],[150, 150, 150],[150, -150, 150],[-150, -150, 150]],
-    cara_s: [[-150, 150, -150],[150, 150, -150],[150, -150, -150],[-150, -150, -150]],
-    cara_izq: [[-150, 150, 150], [-150, 150, -150], [-150, -150, -150],[-150, -150, 150]],
-    cara_der: [[150, 150, 150], [150, 150, -150], [150, -150, -150],[150, -150, 150]],
+    cara_inferior: [[-150, 150, 150],[150, 150, 150],[150, -150, 150],[-150, -150, 150]],
+    cara_superior: [[-150, 150, -150],[150, 150, -150],[150, -150, -150],[-150, -150, -150]],
+    cara_izquierda: [[-150, 150, 150], [-150, 150, -150], [-150, -150, -150],[-150, -150, 150]],
+    cara_derecha: [[150, 150, 150], [150, 150, -150], [150, -150, -150],[150, -150, 150]],
     
-    dibujaCubo(ctx, ang, M) {
-        this.cara_i = transformacionRot3D(this.cara_i, ang, M);
-        this.cara_s = transformacionRot3D(this.cara_s, ang, M);
-        this.cara_izq = transformacionRot3D(this.cara_izq, ang, M);
-        this.cara_der = transformacionRot3D(this.cara_der, ang, M);
+    dibujaCubo(ctx, a, b, g) {
+        let cara_i = transformacionRot3D(this.cara_inferior, a, b, g);
+        let cara_s = transformacionRot3D(this.cara_superior, a, b, g);
+        let cara_izq = transformacionRot3D(this.cara_izquierda, a, b, g);
+        let cara_der = transformacionRot3D(this.cara_derecha, a, b, g);
         ctx.clearRect(0, 0, 500, 500);
-        dibuja2d(ctx, transformacionR3aR2(this.cara_i));
-        dibuja2d(ctx, transformacionR3aR2(this.cara_s));
-        dibuja2d(ctx, transformacionR3aR2(this.cara_izq));
-        dibuja2d(ctx, transformacionR3aR2(this.cara_der));
+        dibuja2d(ctx, transformacionR3aR2(cara_i));
+        dibuja2d(ctx, transformacionR3aR2(cara_s));
+        dibuja2d(ctx, transformacionR3aR2(cara_izq));
+        dibuja2d(ctx, transformacionR3aR2(cara_der));
     }
 }
 
@@ -88,40 +91,23 @@ const input_a = document.getElementById("alpha");
 const input_b = document.getElementById("beta"); 
 const input_g = document.getElementById("gamma"); 
 
-cubo.dibujaCubo(ctx2d, 0, [[1, 0, 0], [0, 0, 1], [0, 1, 0]]);
+let alpha=0;
+let beta=0;
+let gamma=0;
+
+cubo.dibujaCubo(ctx2d, 0, 0, 0);
 
 input_a.addEventListener("input", () => { // Detecta el cambio en el angulo.
-    const ang = (input_a.value/180 )* Math.PI; // Obteniendo el angulo y convirtiendolo a radianes
-    
-    console.log(ang)
-    // Matriz que rota con respecto al eje x
-    const mat_rotacion_x = [[1, 0, 0],      
-                            [0, Math.sin(ang), Math.cos(ang)],
-                            [0, Math.cos(ang), -Math.sin(ang)]];
-
-    console.log(mat_rotacion_x)
-
-    cubo.dibujaCubo(ctx2d, ang, mat_rotacion_x);
+    alpha = (input_a.value/180 )* Math.PI; // Obteniendo el angulo y convirtiendolo a radianes
+    cubo.dibujaCubo(ctx2d, alpha, beta, gamma);
 });
 
 input_b.addEventListener("input", () => { // Detecta el cambio en el angulo.
-    const ang = (input_b.value/180 )* Math.PI; // Obteniendo el angulo y convirtiendolo a radianes
-    
-    // Matriz que rota con respecto al eje y
-    const mat_rotacion_y = [[Math.cos(ang), 0, Math.sin(ang)],
-                        [0, 1, 0],
-                        [-Math.sin(ang), 0, Math.cos(ang)]];
-
-    cubo.dibujaCubo(ctx2d, ang, mat_rotacion_y);
+    beta = (input_b.value/180 )* Math.PI; // Obteniendo el angulo y convirtiendolo a radianes
+    cubo.dibujaCubo(ctx2d, alpha, beta, gamma);
 });
 
 input_g.addEventListener("input", () => { // Detecta el cambio en el angulo.
-    const ang = (input_g.value/180 )* Math.PI; // Obteniendo el angulo y convirtiendolo a radianes
-
-    // Matriz que rota con respecto al eje z 
-    const mat_rotacion_z = [[Math.cos(ang),  -Math.sin(ang), 0], 
-                            [Math.sin(ang), Math.cos(ang), 0],
-                            [0, 0, 1]];
-
-    cubo.dibujaCubo(ctx2d, ang, mat_rotacion_z);
+    gamma = (input_g.value/180 )* Math.PI; // Obteniendo el angulo y convirtiendolo a radianes
+    cubo.dibujaCubo(ctx2d, alpha, beta, gamma);
 });
